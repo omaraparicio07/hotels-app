5f96f8ce8e9ad8c60a3be676f406e3c2dd199930
OmarApQz <omaraparicio07@gmail.com>
2024-03-29 16:43:31 -0600

    Add simple home page

---
+++ src/views/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = (props) => {
+  const { loggedIn, email } = props
+  const navigate = useNavigate()
+
+  const onButtonClick = () => {
+    // You'll update this function later
+  }
+
+  return (
+    <div className="mainContainer">
+      <div className={'titleContainer'}>
+        <div>Welcome!</div>
+      </div>
+      <div>Pagina de inicio para el proyecto de hotelsApp</div>
+      <div className={'buttonContainer'}>
+        <input
+          className={'inputButton'}
+          type="button"
+          onClick={onButtonClick}
+          value={loggedIn ? 'Cerrar sesión' : 'Iniciar sesión'}
+        />
+        {loggedIn ? <div>Tu correo electrónico es {email}</div> : <div />}
+      </div>
+    </div>
+  )
+}
+
+export default Home
